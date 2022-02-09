53f2ee7ed8be042bc16a34419e5bc6b63b07dd78
spicyShoyo <zliu263@gmail.com>
2022-02-08 20:04:42 -0800

    ui: add tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,9 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
+    <div className="App font-bold underline">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <header className="App-header">
 	<img src={logo} className="App-logo" alt="logo" />
 	<p>Hello Vite + asjiddAReact!</p>
