102a43d6ca4735bfa418822a4ed1c3f435dabd27
spicyShoyo <zliu263@gmail.com>
2022-02-10 20:16:44 -0800

    ui: add eslint and prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
+import logo from "@/logo.svg";
+import "@/App.css";
 
 function App() {
   const [count, setCount] = useState(0);
