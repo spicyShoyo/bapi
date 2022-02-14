449ad2b37c3a765b9cc320c4a9b798bc21964e7d
spicyShoyo <zliu263@gmail.com>
2022-02-13 20:11:07 -0800

    ui: layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,51 +1,54 @@
 import axios from "axios";
-import { useState } from "react";
 import tw from "tailwind-styled-components";
 
 const Header = tw.div`
- bg-sky-900	min-h-screen flex items-center justify-center flex-col text-white text-lg
+h-[32px] text-white text-lg text-left pl-4 font-logo
+`;
+const SidePane = tw.div`
+w-[324px]  bg-sky-500 min-h-full
+`;
+const ResultView = tw.div`
+ bg-sky-300 grow min-h-full
 `;
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
-    <div className="text-center">
-      <Header>
-        <p>Bapi</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-          <br />
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
-            onClick={() => {
-              const useProd = new URL(
-                document.location.toString(),
-              ).searchParams.has("prod");
-              const path = useProd ? "https://bapi.io" : "";
-              axios
-                .get(`${path}/v1/queryRows`, {
-                  params: {
-                    q: {
-                      min_ts: 1641672504,
-                      int_column_names: ["ts"],
-                      str_column_names: ["event"],
+    <div className="text-center bg-sky-900	min-h-screen  flex flex-col">
+      <Header>Bapi</Header>
+      <div className="min-w-screen flex grow">
+        <SidePane>
+          <p>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+              onClick={() => {
+                const useProd = new URL(
+                  document.location.toString(),
+                ).searchParams.has("prod");
+                const path = useProd ? "https://bapi.io" : "";
+                axios
+                  .get(`${path}/v1/queryRows`, {
+                    params: {
+                      q: {
+                        min_ts: 1641672504,
+                        int_column_names: ["ts"],
+                        str_column_names: ["event"],
+                      },
                     },
-                  },
-                })
-                .then((v) => {
+                  })
+                  .then((v) => {
+                    // eslint-disable-next-line
+                    console.log(v);
+                  })
                   // eslint-disable-next-line
-                  console.log(v);
-                })
-                // eslint-disable-next-line
-                .catch((e) => console.log(e));
-            }}
-          >
-            Load
-          </button>
-        </p>
-      </Header>
+                  .catch((e) => console.log(e));
+              }}
+            >
+              Load
+            </button>
+          </p>
+        </SidePane>
+        <ResultView />
+      </div>
     </div>
   );
 }
